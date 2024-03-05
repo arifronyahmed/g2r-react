355333543da69d187ca4daa2eeac6b7ef4414bd0
Arif <arifronyahmed@gmail.com>
2024-03-05 16:23:36 +0100

    Contact form added

--- src/components/homePage/Form/Form.jsx
+++ src/components/homePage/Form/Form.jsx
@@ -1,3 +1,21 @@
+import styles from "./Form.module.css";
+
 export default function Form() {
-  return <div>Form</div>;
+  return (
+    <form className={styles.form}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" placeholder="*Arif Ahmed" />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" placeholder="*arifahmed@gmail.com" />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <input type="text" id="message" placeholder="*arifahmed@gmail.com" />
+      </div>
+      <button className={`${styles.form_btn} btn`}>Submit</button>
+    </form>
+  );
 }
